ea8b5018e174149d7336e4eec910ca3e38c7cd1f
harishtpj <harishtpj@hotmail.com>
2023-04-11 14:06:02 +0530

    Added support to return exit code from docker prgm

--- app/main.py
+++ app/main.py
@@ -9,6 +9,7 @@ def main():
     completed_process = subprocess.run([command, *args], capture_output=True)
     sys.stdout.write(completed_process.stdout.decode("utf-8"))
     sys.stderr.write(completed_process.stderr.decode("utf-8"))
+    sys.exit(completed_process.returncode)
 
 
 if __name__ == "__main__":
