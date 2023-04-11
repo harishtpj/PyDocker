35b4e1531eeaac9f6d1d6488c2b697f7986bd49f
harishtpj <harishtpj@hotmail.com>
2023-04-11 14:01:41 +0530

    Piped STDOUT and STDERR

--- app/main.py
+++ app/main.py
@@ -5,9 +5,10 @@ import sys
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
-    
+
     completed_process = subprocess.run([command, *args], capture_output=True)
-    print(completed_process.stdout.decode("utf-8"))
+    sys.stdout.write(completed_process.stdout.decode("utf-8"))
+    sys.stderr.write(completed_process.stderr.decode("utf-8"))
 
 
 if __name__ == "__main__":
