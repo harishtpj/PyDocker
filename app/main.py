e52b92324a7f08515daa8bc965e491d41cba32f7
harishtpj <harishtpj@hotmail.com>
2023-04-12 19:28:35 +0530

    Added support for virtual PID and chroot

--- app/main.py
+++ app/main.py
@@ -1,14 +1,27 @@
-import subprocess
-import sys
+import subprocess, sys, os, tempfile, shutil
+from ctypes import *
 
+CLONE_NEWPID = 0x20000000
 
 def main():
     command = sys.argv[3]
     args = sys.argv[4:]
 
-    completed_process = subprocess.run([command, *args], capture_output=True)
-    sys.stdout.write(completed_process.stdout.decode("utf-8"))
-    sys.stderr.write(completed_process.stderr.decode("utf-8"))
+    libc = CDLL("libc.so.6")
+    jdirs = ["usr/bin", "usr/local/bin", "lib/x86_64-linux-gnu", "lib64"]
+    pid_ns = libc.unshare(CLONE_NEWPID)
+
+    with tempfile.TemporaryDirectory() as jail:
+        os.chdir(jail)
+        for j_dir in jdirs:
+            os.makedirs(j_dir)
+        shutil.copy("/usr/local/bin/docker-explorer", "usr/local/bin/")
+        os.chroot(".")
+
+        completed_process = subprocess.run([command, *args], capture_output=True)
+        sys.stdout.write(completed_process.stdout.decode("utf-8"))
+        sys.stderr.write(completed_process.stderr.decode("utf-8"))
+    
     sys.exit(completed_process.returncode)
 
 
