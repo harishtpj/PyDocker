54b2fbc7457860d2ea7f0e0ff31a0f3159f87b00
harishtpj <harishtpj@hotmail.com>
2023-04-15 18:40:24 +0530

    Support for extracting img from docker registry

--- app/main.py
+++ app/main.py
@@ -1,21 +1,98 @@
-import subprocess, sys, os, tempfile, shutil
+import subprocess, sys, os, tempfile, shutil, json, tarfile
 from ctypes import *
+from urllib import request
+from urllib.error import HTTPError
+from pprint import pprint
 
 CLONE_NEWPID = 0x20000000
 
+def get_manifest(name, reference):
+    url = f"https://registry.hub.docker.com/v2/{name}/manifests/{reference}"
+    try:
+        r = request.urlopen(url)
+        j = json.loads(r.read())
+        return j
+    except HTTPError as http_error:
+        if http_error.reason == "Unauthorized":
+            req = request.Request(url)
+            token = get_token(name, "pull")
+            req.add_header("Authorization", f"Bearer {token}")
+            req.add_header("Accept", "application/vnd.oci.image.index.v1+json")
+            r = request.urlopen(req)
+            j = json.loads(r.read())
+            return j
+        else:
+            print(f"HTTPError: {http_error}")
+    except Exception as e:
+        print(f"Exception: {e}")
+
+    return j
+
+def pull_layer(name, digest):
+    filename = digest.split(":")[1] + ".tar"
+    url = f"https://registry.hub.docker.com/v2/{name}/blobs/{digest}"
+    try:
+        r = request.urlopen(url)
+        if r.getcode() == 307:
+            url = r.getheader("Location")
+            r = request.urlopen(url)
+            with open(filename, "bw") as f:
+                f.write(r.read())
+            tar = tarfile.open(filename)
+            os.remove(filename)
+            tar.extractall()
+    except HTTPError as http_error:
+        if http_error.reason == "Unauthorized":
+            req = request.Request(url)
+            token = get_token(name, "pull")
+            req.add_header("Authorization", f"Bearer {token}")
+            r = request.urlopen(req)
+            with open(filename, "bw") as f:
+                f.write(r.read())
+
+            tar = tarfile.open(filename)
+            tar.extractall()
+            os.remove(filename)
+    except Exception as e:
+        print(e)
+
+
+def get_token(name, action):
+    name = name.replace("/", "%2F")
+    r = request.urlopen(
+        f"https://auth.docker.io/token?scope=repository:{name}:{action}&service=registry.docker.io"
+    )
+    j = json.loads(r.read())
+    return j["token"]
+
 def main():
-    command = sys.argv[3]
-    args = sys.argv[4:]
+    image = sys.argv[2]
+    try:
+        image_name, image_ref = image.split(":")
+    except:
+        image_name, image_ref = image, "latest"
+
+    image_name = f"library/{image_name}"
+    command, args = sys.argv[3], sys.argv[4:]
 
     libc = CDLL("libc.so.6")
-    jdirs = ["usr/bin", "usr/local/bin", "lib/x86_64-linux-gnu", "lib64"]
     pid_ns = libc.unshare(CLONE_NEWPID)
 
     with tempfile.TemporaryDirectory() as jail:
         os.chdir(jail)
-        for j_dir in jdirs:
-            os.makedirs(j_dir)
-        shutil.copy("/usr/local/bin/docker-explorer", "usr/local/bin/")
+
+        manifests = get_manifest(image_name, image_ref)
+        if manifests["schemaVersion"] == 2:
+            layers = manifests["manifests"]
+            for layer in layers:
+                digest = layer["digest"]
+                pull_layer(image_name, digest)
+        else:
+            layers = manifests["fsLayers"]
+            for layer in layers:
+                digest = layer["blobSum"]
+                pull_layer(image_name, digest)
+
         os.chroot(".")
 
         completed_process = subprocess.run([command, *args], capture_output=True)
